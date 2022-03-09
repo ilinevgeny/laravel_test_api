84351ab661466c3ec0d47caf32aac10a0aa90711
Ev.Il <xzenya@gmail.com>
2022-03-09 12:26:09 +0100

    init proj; create Arctilce model; implement api controller;

---
+++ routes/api.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Models\Article;
+use App\Http\Controllers\ArticleController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Route::get('articles', function() {
+//    // If the Content-Type and Accept headers are set to 'application/json',
+//    // this will return a JSON structure. This will be cleaned up later.
+//    return Article::all();
+//});
+//
+//Route::get('articles/{id}', function($id) {
+//    return Article::find($id);
+//});
+//
+//Route::post('articles', function(Request $request) {
+//    return Article::create($request->all);
+//});
+//
+//Route::put('articles/{id}', function(Request $request, $id) {
+//    $article = Article::findOrFail($id);
+//    $article->update($request->all());
+//
+//    return $article;
+//});
+//
+//Route::delete('articles/{id}', function($id) {
+//    Article::find($id)->delete();
+//
+//    return 204;
+//});
+
+Route::get('articles', 'App\Http\Controllers\ArticleController@index');
+Route::get('articles/{article}', 'App\Http\Controllers\ArticleController@show');
+Route::post('articles', 'App\Http\Controllers\ArticleController@store');
+Route::put('articles/{article}', 'App\Http\Controllers\ArticleController@update');
+Route::delete('articles/{article}', 'App\Http\Controllers\ArticleController@delete');
